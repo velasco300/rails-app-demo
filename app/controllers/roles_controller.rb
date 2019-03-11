e518716a0c5fddf65df8c205dd70dde21a9422b9
wwh <wwh@wwh.com>
2019-03-11 03:06:54 +0000

    modify code

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,50 +1,41 @@
 class RolesController < ApplicationController
   before_action :set_role, only: [:show, :update, :destroy]
 
-  # GET /roles
   def index
     @roles = Role.all
-
-    render json: @roles
+    success_msg @roles
   end
 
-  # GET /roles/1
   def show
-    render json: @role
+    success @role
   end
 
-  # POST /roles
   def create
     @role = Role.new(role_params)
-
     if @role.save
-      render json: @role, status: :created, location: @role
+      success_msg @role
     else
-      render json: @role.errors, status: :unprocessable_entity
+      err_msg @role.errors.messages
     end
   end
 
-  # PATCH/PUT /roles/1
   def update
     if @role.update(role_params)
-      render json: @role
+      success @role
     else
-      render json: @role.errors, status: :unprocessable_entity
+      err_msg @role.errors.messages
     end
   end
 
-  # DELETE /roles/1
   def destroy
     @role.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_role
       @role = Role.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def role_params
       params.require(:role).permit(:nick_name)
     end
