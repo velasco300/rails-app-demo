35378e457df548155cd4211afe25f065610fdc6a
wwh <wwh@wwh.com>
2019-03-11 07:36:22 +0000

    add config file suport

---
+++ config/initializers/redis.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+require 'redis'
+
+# 读取redis配置文件
+redis = Rails.application.config_for(:redis).symbolize_keys!
+redis_server = redis[:redis_server]
+redis_port   = redis[:redis_port]
+redis_db_num = redis[:redis_db_num]
+
+# 实例化全局对象 $redis
+$redis = Redis.new(host: redis_server, port: redis_port, db: redis_db_num)
