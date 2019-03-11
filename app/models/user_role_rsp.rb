50a18f55a72d7de94f5b493274248dde3dd25e9a
wwh <wwh@wwh.com>
2019-03-11 03:07:29 +0000

    modify code

--- app/models/user_role_rsp.rb
+++ app/models/user_role_rsp.rb
@@ -1,4 +1,9 @@
+# coding: utf-8
 class UserRoleRsp < ApplicationRecord
+  validates :role, presence: { message: '不能为空' }
+  validates :user, presence: { message: '不能为空' }
+  validates :user, uniqueness: { scope: :role, message: '记录已经存在' }
+  
   belongs_to :user
   belongs_to :role
 end
